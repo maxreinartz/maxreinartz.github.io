e6bda8e77ecf4a2c9aa2659c054f6b725f1916c0
Creeper76 <86135033+Creeper76@users.noreply.github.com>
2024-02-23 22:16:41 -0500

    Fix a few issues

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 window.onload = function () {
   var body = document.body;
 
-  // Check for saved theme in local storage
   var savedTheme = localStorage.getItem("theme");
   if (savedTheme) {
     body.className = savedTheme;
+    if (savedTheme === "dark-mode") {
+      document.getElementById('theme-toggle').querySelector('.fa-sun').style.display = 'inline-block';
+      document.getElementById('theme-toggle').querySelector('.fa-moon').style.display = 'none';
+    } else {
+      document.getElementById('theme-toggle').querySelector('.fa-sun').style.display = 'none';
+      document.getElementById('theme-toggle').querySelector('.fa-moon').style.display = 'inline-block';
+    }
   }
 };
 
@@ -20,11 +26,13 @@ document.getElementById('theme-toggle').addEventListener('click', function() {
     body.style.transition = 'all 0.3s ease';
     body.classList.remove('dark-mode');
     body.classList.add('light-mode');
+    localStorage.setItem("theme", "light-mode");
   } else {
     sunIcon.style.display = 'inline-block';
     moonIcon.style.display = 'none';
     body.style.transition = 'all 0.3s ease';
     body.classList.remove('light-mode');
     body.classList.add('dark-mode');
+    localStorage.setItem("theme", "dark-mode");
   }
 });
